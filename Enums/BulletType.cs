b2036084f1aded2cca4b3ae2a6b0c288a4fb4913
AslanAbdulrazagov <aslanabdulrazaqov496@gmail.com>
2024-07-16 17:54:50 +0400

    Add project files.

---
+++ Enums/BulletType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum BulletType
+    {
+        mm_556,
+        mm_762,
+        mm_900
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Enums
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Weapon myWeapon = new Weapon(BulletType.mm_762, 5);
+            myWeapon.Fill();
+
+            myWeapon.PullTrigger(); 
+            myWeapon.Fire();       
+            myWeapon.Fire();        
+
+            myWeapon.PullTrigger(); 
+
+
+            
+        }
+    }
+}
